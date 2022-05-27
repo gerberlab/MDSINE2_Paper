75a0cddab075723c5f37048070c30b31b922f30d
Younhun Kim <younhun@mit.edu>
2022-05-27 14:30:05 -0400

    Filled in missing "export" invocation in settings.sh.

--- scripts/settings.sh
+++ scripts/settings.sh
@@ -1,6 +1,6 @@
 # MODIFY THESE BASED ON LOCAL ENVIRONMENT.
-PROJECT_DIR="/mnt/d/Projects/MDSINE2_figures"
-DATASET_NAME="gibson"
+export PROJECT_DIR="/mnt/d/Projects/MDSINE2_figures"
+export DATASET_NAME="gibson"
 
 
 # =================== DON'T MODIFY THESE (unless you really need to)
@@ -8,13 +8,13 @@ _THIS_PATH="${PROJECT_DIR}/scripts/settings.sh"  # the location of this file.
 echo "[*] Using environment settings from ${_THIS_PATH}."
 
 
-DATASET_DIR="${PROJECT_DIR}/datasets/${DATASET_NAME}"
-PREPROCESS_DIR="${DATASET_DIR}/preprocessed"
+export DATASET_DIR="${PROJECT_DIR}/datasets/${DATASET_NAME}"
+export PREPROCESS_DIR="${DATASET_DIR}/preprocessed"
 
 # === outputs go here
-OUTPUT_DIR="${DATASET_DIR}/output"
-PHYLOGENY_OUT_DIR="${OUTPUT_DIR}/phylogeny"
-PLOTS_OUT_DIR="${OUTPUT_DIR}/plots"
+export OUTPUT_DIR="${DATASET_DIR}/output"
+export PHYLOGENY_OUT_DIR="${OUTPUT_DIR}/phylogeny"
+export PLOTS_OUT_DIR="${OUTPUT_DIR}/plots"
 
 
 require_program()
