a6f75c777550b3d56e68d6c1f5f94befc99b365e
Younhun Kim <younhun@mit.edu>
2022-06-01 12:02:22 -0400

    added logging INI file.

--- scripts/runtime_benchmark/settings.sh
+++ scripts/runtime_benchmark/settings.sh
@@ -6,6 +6,7 @@ echo "[*] Using additional settings from ${_THIS_PATH}"
 
 export OUTPUT_DIR=${OUTPUT_DIR}/runtime_benchmark
 
+export MDSINE2_LOG_INI="${PROJECT_DIR}/scripts/runtime_benchmark/logging.ini"
 export MDSINE2_NUM_TRIALS=5
 export MDSINE2_OUTPUT_DIR=${OUTPUT_DIR}/mdsine2
 export MDSINE2_RUNTIMES=${MDSINE2_OUTPUT_DIR}/runtimes.txt
