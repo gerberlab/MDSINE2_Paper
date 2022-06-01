94fa53f1e2b9777862395f33316df6e6b3e8d681
Younhun Kim <younhun@mit.edu>
2022-06-01 14:21:19 -0400

    fixed broken pathing in seetings.sh for eristwo script.

--- job_scheduling/eristwo/runtime_benchmark/settings.sh
+++ job_scheduling/eristwo/runtime_benchmark/settings.sh
@@ -2,7 +2,7 @@
 export PROJECT_DIR="/mnt/d/Projects/MDSINE2_figures"
 
 # ========= Don't modify below
-source ${PROJECT_DIR}/settings/runtime_benchmark/settings.sh
+source ${PROJECT_DIR}/scripts/runtime_benchmark/settings.sh
 
 export LSF_GROUP=LSF_JOB_GROUP
 export LSF_DIR="./lsf_files"
