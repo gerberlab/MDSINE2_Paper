d7c3734826281ddc7931d85ea468d129313c5eb2
Younhun Kim <younhun@mit.edu>
2022-06-07 14:03:09 -0400

    Fixed pathing for mdsine2_infer_all.sh

--- scripts/synthetic/mdsine2_infer_all.sh
+++ scripts/synthetic/mdsine2_infer_all.sh
@@ -7,10 +7,10 @@ for read_depth in 1000 25000; do
 		for noise_level in "low" "medium" "high"; do
 			dataset=${DATASET_DIR}/data/reads_${read_depth}/trial_${trial}/subjset_${noise_level}.pkl
 			trial_output_dir=${OUTPUT_DIR}/reads_${read_depth}/trial_${trial}/${noise_level}_noise
-			inference_out_dir=${trial_output_dir}/inference
+			inference_out_dir=${trial_output_dir}/mdsine2
 			mkdir -p $inference_out_dir
 
-			# ======= Run inference (and compute runtime)
+			# ======= Run inference
 			echo "[*] Running mdsine2 inference (trial=${trial}, noise level=${noise_level})"
 			python synthetic/helpers/inference.py \
 					--input $dataset \
@@ -24,14 +24,6 @@ for read_depth in 1000 25000; do
 					--interaction-ind-prior "strong-sparse" \
 					--perturbation-ind-prior "weak-agnostic"
 			echo "[*] Finished mdsine2 inference."
-
-			# ======= Draw visualizations.
-			echo "[*] Drawing mdsine2 posterior visualization."
-			mkdir -p $inference_out_dir/posterior
-			mdsine2 visualize-posterior \
-					--chain $inference_out_dir/mcmc.pkl \
-					--output-basepath $inference_out_dir/posterior
-			echo "[*] Finished mdsine2 posterior visualization."
 		done
 	done
 done
