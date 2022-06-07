d563a12da6cea09f147fc061210c839b8c6c615c
Younhun Kim <younhun@mit.edu>
2022-06-07 16:41:49 -0400

    compartmentalized/fixed regression script.

--- scripts/synthetic/run_regression_all.sh
+++ scripts/synthetic/run_regression_all.sh
@@ -7,30 +7,29 @@ for read_depth in 1000 25000; do
 	for (( trial = 0; trial < ${NUM_SAMPLE_TRIALS}; trial++ )); do
 		for noise_level in "low" "medium" "high"; do
 			dataset_file=${DATASET_DIR}/data/reads_${read_depth}/trial_${trial}/subjset_${noise_level}.pkl
-			inference_out_dir=${OUTPUT_DIR}/reads_${read_depth}/trial_${trial}/${noise_level}_noise/${model_name}/${regression_type}
-			mkdir -p $inference_out_dir
+			intermediate_dir=${OUTPUT_DIR}/reads_${read_depth}/trial_${trial}/${noise_level}_noise
+			mkdir -p $intermediate_dir
 
-			echo "[*] Saving output to ${inference_out_dir}"
-			python synthetic/helpers/create_clv_inputs.py -i $dataset_file -o $inference_out_dir
+			echo "[*] Creating CLV files in ${intermediate_dir}"
+			python synthetic/helpers/create_clv_inputs.py -i $dataset_file -o $intermediate_dir
 
 			# ======= Run regression methods
-			echo "[*] Running CLV, elastic-net (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-			python ${CLV_DIR}/healthy_prediction_experiments.py -m "clv" -r "elastic-net" -o $inference_out_dir -i $inference_out_dir
-
-			echo "[*] Running LRA, elastic-net (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-			python ${CLV_DIR}/healthy_prediction_experiments.py -m "lra" -r "elastic-net" -o $inference_out_dir -i $inference_out_dir
-
-			echo "[*] Running GLV, elastic-net (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-			python ${CLV_DIR}/healthy_prediction_experiments.py -m "glv" -r "elastic-net" -o $inference_out_dir -i $inference_out_dir
-
-			echo "[*] Running GLV, ridge (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-			python ${CLV_DIR}/healthy_prediction_experiments.py -m "glv" -r "ridge" -o $inference_out_dir -i $inference_out_dir
-
-			echo "[*] Running GLV-ra, elastic-net (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-			python ${CLV_DIR}/healthy_prediction_experiments.py -m "glv-ra" -r "elastic-net" -o $inference_out_dir -i $inference_out_dir
-
-			echo "[*] Running GLV-ra, ridge (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
-			python ${CLV_DIR}/healthy_prediction_experiments.py -m "glv-ra" -r "ridge" -o $inference_out_dir -i $inference_out_dir
+			run_regression "clv" "elastic_net"
+			run_regression "lra" "elastic_net"
+			run_regression "glv" "elastic_net"
+			run_regression "glv" "ridge"
+			run_regression "glv-ra" "elastic_net"
+			run_regression "glv-ra" "ridge"
 		done
 	done
 done
+
+
+run_regression() {
+	model=$1
+	regression_type=$2
+	echo "[*] Running ${model}, ${regression_type} (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
+	out_dir=$intermediate_dir/$model/$regression_type
+	mkdir -p $out_dir
+	python ${CLV_DIR}/healthy_prediction_experiments.py -m $model -r $regression_type -o $out_dir -i $intermediate_dir
+}
