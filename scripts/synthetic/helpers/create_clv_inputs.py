542a99fb1741fac16ded7f6688ad27fac2efa5d9
Younhun Kim <55497373+yk23@users.noreply.github.com>
2022-06-09 22:50:19 -0400

    Synthetic experiment code (#7)
    * Implemented plain regression methods for synthetic.
    
    * Filled in skeleton of evaluate.py, but some TODOs left.

---
+++ scripts/synthetic/helpers/create_clv_inputs.py
@@ -0,0 +1,127 @@
+"""
+generates the data in a format that is compatible to work with the original cLV code
+"""
+import numpy as np
+import pickle as pkl
+import argparse
+from pathlib import Path
+
+import mdsine2 as md2
+from scipy.interpolate import UnivariateSpline
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--mdsine2_study", required=True)
+    parser.add_argument("-o", "--output_dir", required=True)
+    return parser.parse_args()
+
+
+def compute_breakpoints(effects):
+    """Break the spline when external perturbations occur."""
+    breakpoints = []
+    for u in effects:
+        in_perturb = False
+        v = []
+        for i,ut in enumerate(u):
+            if i == 0 or i == u.shape[0]-1:
+                v.append(1)
+                continue
+            if np.any(ut) > 0 and not in_perturb:
+                v.append(1)
+                in_perturb = True
+            elif np.any(ut) > 0 and in_perturb:
+                v.append(0)
+            elif np.all(ut) == 0 and in_perturb:
+                i = 1 if v[i-1] == 0 else 0
+                v.append(i)
+                in_perturb = False
+            else:
+                v.append(0)
+        v = np.array(v)
+        breakpoints.append(np.nonzero(v)[0])
+    return breakpoints
+
+
+def denoise(counts, t_pts, effects=None):
+    """Takes a sequence of counts at t_pts, and returns denoised estimates
+    of latent trajectories."""
+    ntaxa = counts[0].shape[1]
+    denoised_traj = []
+
+    if effects is not None:
+        breakpoints = compute_breakpoints(effects)
+        for c,t,b in zip(counts,t_pts,breakpoints):
+            denoised = np.zeros(c.shape)
+            mass = c.sum(axis=1,keepdims=True)
+            p = c / c.sum(axis=1,keepdims=True)
+            p[p==0] = 1e-5
+            p /= p.sum(axis=1,keepdims=True)
+            c = (mass.T*p.T).T
+            for i in range(ntaxa):
+                for j in range(1,b.size):
+                    start = b[j-1]
+                    end = b[j]+1
+                    k = 5 if end - start <= 3 else 5
+                    f = UnivariateSpline(t[start:end],c[start:end,i],k=k)
+                    denoised[start:end,i] = f(t[start:end])
+            denoised[0] = c[0]
+            denoised = np.clip(denoised, np.min(denoised[denoised > 0]), np.inf)
+            denoised_traj.append(denoised)
+    else:
+        for c, t in zip(counts,t_pts):
+            denoised = np.zeros(c.shape)
+            k = 3 if t.shape[0] <= 5 else 5
+            for i in range(ntaxa):
+                f = UnivariateSpline(t,c[:,i],k=k)
+                denoised[:,i] = f(t)
+            denoised = np.clip(denoised, np.min(denoised[denoised > 0]), np.inf)
+            denoised /= denoised.sum(axis=1,keepdims=True)
+            denoised_traj.append(denoised)
+
+    return denoised_traj
+
+
+if __name__ == "__main__":
+    sample_id_to_subject_id = {}
+    subject_id_time = {}
+    subject_id_u = {}
+
+    args = parse_arguments()
+    study = md2.Study.load(args.mdsine2_study)
+    taxa = study.taxa
+
+    subject_counts = {
+        subj.name: subj.matrix()["abs"] for subj in study
+    }
+
+    Y = []
+    U = []
+    T = []
+    zero_counts = 0
+    total_counts = 0
+
+    for subj in study:
+        counts = subj.matrix()["abs"].T
+        times = subj.times
+        perturb_ids = np.zeros(shape=(len(times), 1))
+
+        assert len(times) == counts.shape[0]
+        assert counts.shape[1] == len(taxa)
+
+        zero_counts += np.sum(counts == 0)
+        total_counts += counts.size
+        Y.append(counts)
+        U.append(perturb_ids)
+        T.append(times)
+
+    Y_denoised = denoise(Y, T, effects=U)
+    out_dir = Path(args.output_dir)
+    with open(out_dir / "Y.pkl", "wb") as f:
+        pkl.dump(Y, f)
+    with open(out_dir / "Y_denoised.pkl", "wb") as f:
+        pkl.dump(Y_denoised, f)
+    with open(out_dir / "U.pkl", "wb") as f:
+        pkl.dump(U, f)
+    with open(out_dir / "T.pkl", "wb") as f:
+        pkl.dump(T, f)
--- scripts/synthetic/helpers/evaluate.py
+++ scripts/synthetic/helpers/evaluate.py
@@ -1,16 +1,228 @@
+from typing import Tuple, Iterator
+from pathlib import Path
 import argparse
+
+import numpy as np
+import pandas as pd
+import mdsine2 as md2
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser()
+    parser.add_argument('-g', '--ground_truth_params', type=str, required=True,
+                        help='<Required> The path to a .npz file containing `growth_rates`, `interactions` arrays')
     return parser.parse_args()
 
 
-def
+# =============== Iterators through directory structures ==========
+def result_dirs(results_base_dir: Path) -> Iterator[Tuple[int, int, str, Path]]:
+    for read_depth, read_depth_dir in read_depth_dirs(results_base_dir):
+        for trial_num, trial_dir in trial_dirs(read_depth_dir):
+            for noise_level, noise_level_dir in noise_level_dirs(trial_dir):
+                yield read_depth, trial_num, noise_level, noise_level_dir
+
+
+def read_depth_dirs(results_base_dir: Path) -> Iterator[Tuple[int, Path]]:
+    for child_dir in results_base_dir.glob("reads_*"):
+        if not child_dir.is_dir():
+            raise RuntimeError(f"Expected child `{child_dir}` to be a directory.")
+
+        read_depth = int(child_dir.name.split("_")[1])
+        yield read_depth, child_dir
+
+
+def trial_dirs(read_depth_dir: Path) -> Iterator[Tuple[int, Path]]:
+    for child_dir in read_depth_dir.glob("trial_*"):
+        if not child_dir.is_dir():
+            raise RuntimeError(f"Expected child `{child_dir}` to be a directory.")
+
+        trial_num = int(child_dir.name.split("_")[1])
+        yield trial_num, child_dir
+
+
+def noise_level_dirs(trial_dir: Path) -> Iterator[Tuple[str, Path]]:
+    for child_dir in trial_dir.glob("*_noise"):
+        if not child_dir.is_dir():
+            raise RuntimeError(f"Expected child `{child_dir}` to be a directory.")
+
+        noise_level = child_dir.name.split("_")[0]
+        yield noise_level, child_dir
+
+
+# ========================= Method output iterators =======================
+def mdsine_output(result_dir: Path) -> md2.BaseMCMC:
+    """
+    Generator which yields MDSINE2 study objects for each trial.
+    :return:
+    """
+    return md2.BaseMCMC.load(str(result_dir / "mdsine2" / "TODOPATH.pkl"))
+
+
+def regression_output(result_dir: Path, model_name: str, regression_type: str) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Generator which yields growth/interaction params estimated through regression.
+    :param result_dir:
+    :return:
+    """
+    result_path = result_dir / model_name / regression_type / "TODOPATH.pkl"
+    # TODO
+    est_growth = None
+    est_interactions = None
+    return est_growth, est_interactions
+
+
+# ======================== Error metrics =======================
+def parse_ground_truth_params(params_path: Path) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    params = np.load(str(params_path))
+    growth = params['growth_rates']
+    interactions = params['interactions']
+    indicators = (interactions != 0.0)
+    return growth, interactions, indicators
+
+
+def evaluate_growth_rate_errors(true_growth: np.ndarray, results_base_dir: Path) -> pd.DataFrame:
+    df_entries = []
+
+    def _error_metric(pred, truth) -> float:
+        return np.sqrt(np.mean(np.square(pred - truth)))
+
+    for read_depth, trial_num, noise_level, result_dir in result_dirs(results_base_dir):
+        def _add_entry(_method: str, _err: float):
+            df_entries.append({
+                'Method': _method,
+                'ReadDepth': read_depth,
+                'Trial': trial_num,
+                'NoiseLevel': noise_level,
+                'Error': _err
+            })
+
+        def _add_regression_entry(_method: str, _regression_type: str):
+            pred_growth, _ = regression_output(result_dir, _method, _regression_type)
+            _add_entry(f'{_method}-{_regression_type}', _error_metric(pred_growth, true_growth))
+
+        # MDSINE2 inference error eval
+        mcmc = mdsine_output(result_dir)
+        pred_growth = np.zeros(1)  # TODO: parse inferred growth rate matrix
+        _add_entry('MDSINE2', _error_metric(pred_growth, true_growth))
+
+        # CLV inference error eval
+        _add_regression_entry("clv", "elastic_net")
+        _add_regression_entry("lra", "elastic_net")
+        _add_regression_entry("glv", "elastic_net")
+        _add_regression_entry("glv", "ridge")
+        _add_regression_entry("glv-ra", "elastic_net")
+        _add_regression_entry("glv-ra", "ridge")
+
+    return pd.DataFrame(df_entries)
+
+
+def evaluate_interaction_strength_errors(true_interactions: np.ndarray, results_base_dir: Path) -> pd.DataFrame:
+    df_entries = []
+
+    def _error_metric(pred, truth) -> float:
+        return np.sqrt(np.mean(np.square(pred - truth)))
+
+    for read_depth, trial_num, noise_level, result_dir in result_dirs(results_base_dir):
+        def _add_entry(_method: str, _err: float):
+            df_entries.append({
+                'Method': _method,
+                'ReadDepth': read_depth,
+                'Trial': trial_num,
+                'NoiseLevel': noise_level,
+                'Error': _err
+            })
+
+        def _add_regression_entry(_method: str, _regression_type: str):
+            _, pred_interaction = regression_output(result_dir, _method, _regression_type)
+            _add_entry(f'{_method}-{_regression_type}', _error_metric(pred_interaction, true_interactions))
+
+        # MDSINE2 inference error eval
+        mcmc = mdsine_output(result_dir)
+        pred_interaction = np.zeros(1)  # TODO: parse inferred growth rate matrix
+        _add_entry('MDSINE2', _error_metric(pred_interaction, true_interactions))
+
+        # CLV inference error eval
+        _add_regression_entry("clv", "elastic_net")
+        _add_regression_entry("lra", "elastic_net")
+        _add_regression_entry("glv", "elastic_net")
+        _add_regression_entry("glv", "ridge")
+        _add_regression_entry("glv-ra", "elastic_net")
+        _add_regression_entry("glv-ra", "ridge")
+
+    return pd.DataFrame(df_entries)
+
+
+def evaluate_topology_errors(true_indicators: np.ndarray, results_base_dir: Path) -> pd.DataFrame:
+    df_entries = []
+
+    def _false_positive_rate(pred, truth) -> float:
+        return np.sum(pred & np.logical_not(truth)) / len(truth)
+
+    def _true_positive_rate(pred, truth) -> float:
+        raise np.sum(pred & truth) / len(truth)
+
+    for read_depth, trial_num, noise_level, result_dir in result_dirs(results_base_dir):
+        def _add_entry(_method: str, _fpr: float, _tpr: float):
+            df_entries.append({
+                'Method': _method,
+                'ReadDepth': read_depth,
+                'Trial': trial_num,
+                'NoiseLevel': noise_level,
+                'FPR': _fpr,
+                'TPR': _tpr
+            })
+
+        def _add_regression_entry(_method: str, _regression_type: str):
+            raise NotImplementedError()
+
+        # MDSINE2 inference error eval
+        mcmc = mdsine_output(result_dir)
+        pred_indicators = np.zeros(1)  # TODO: parse inferred growth rate matrix
+        _add_entry(
+            'MDSINE2',
+            _false_positive_rate(pred_indicators, true_indicators),
+            _true_positive_rate(pred_indicators, true_indicators)
+        )
+
+        # CLV inference error eval
+        _add_regression_entry("clv", "elastic_net")
+        _add_regression_entry("lra", "elastic_net")
+        _add_regression_entry("glv", "elastic_net")
+        _add_regression_entry("glv", "ridge")
+        _add_regression_entry("glv-ra", "elastic_net")
+        _add_regression_entry("glv-ra", "ridge")
+
+    return pd.DataFrame(df_entries)
+
+
+def evaluate_holdout_trajectory_errors(true_growth: np.ndarray, true_interactions: np.ndarray, results_base_dir: Path) -> pd.DataFrame:
+    raise NotImplementedError()
 
 
 def main():
     args = parse_args()
+    growth, interactions, indicators = parse_ground_truth_params(args.ground_truth_params)
+
+    results_base_dir = Path(args.results_base_dir)
+    output_dir = Path(args.output_dir)
+    output_dir.mkdir(exist_ok=True, parents=True)
+    print(f"Outputs will be saved to {output_dir}.")
+
+    growth_rate_errors = evaluate_growth_rate_errors(growth, results_base_dir)
+    growth_rate_errors.to_csv(output_dir / "growth_rate_errors.csv")
+    print(f"Wrote growth rate errors.")
+
+    interaction_strength_errors = evaluate_interaction_strength_errors(interactions, results_base_dir)
+    interaction_strength_errors.to_csv(output_dir / "interaction_strength_errors.csv")
+    print(f"Wrote interaction strength errors.")
+
+    topology_errors = evaluate_topology_errors(indicators, results_base_dir)
+    topology_errors.to_csv(output_dir / "topology_errors.csv")
+    print(f"Wrote interaction topology errors.")
+
+    holdout_trajectory_errors = evaluate_holdout_trajectory_errors(growth, interactions, results_base_dir)
+    holdout_trajectory_errors.to_csv(output_dir / "holdout_trajectory_errors.csv")
+    print(f"Wrote heldout trajectory prediction errors.")
 
 
 if __name__ == "__main__":
