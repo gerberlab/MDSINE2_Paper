2d86a8d93cd491f03a8fc33d99fdf80ecfda5e7c
Younhun Kim <younhun@mit.edu>
2022-06-15 13:42:37 -0400

    wip

--- scripts/synthetic/helpers/create_mdsine1_inputs.py
+++ scripts/synthetic/helpers/create_mdsine1_inputs.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import argparse
 
 import mdsine2 as md2
@@ -19,10 +20,23 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
+def create_counts(study: md2.Study, counts_path: Path):
+    for sample_id in study._samples:
+        sid, t = study._samples[sample_id]
+        if t not in study[sid].times:
+            continue
+
+        print(sid)
+        print(study[sid].reads[t])
+
+
 def main():
     args = parse_args()
     study = md2.Study.load(args.input_study)
 
+    out_dir = Path(args.out_dir)
+    out_dir.mkdir(exist_ok=True, parents=True)
+    create_counts(study, out_dir / args.counts_file)
 
 
 if __name__ == "__main__":
