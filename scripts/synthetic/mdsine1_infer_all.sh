e04c30a471f3770a982ec3ff8e3d50c649338d71
Younhun Kim <younhun@mit.edu>
2022-06-15 17:04:08 -0400

    added batch script for mdsine1.

---
+++ scripts/synthetic/mdsine1_infer_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+source synthetic/settings.sh
+
+for read_depth in 1000 25000; do
+	for (( trial = 0; trial < ${NUM_SAMPLE_TRIALS}; trial++ )); do
+		for noise_level in "low" "medium" "high"; do
+			bash synthetic/helpers/infer_mdsine1.sh $read_depth $trial $noise_level
+		done
+	done
+done
