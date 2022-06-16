bbb1594a5c2f0402ecc56d7dff1d368734de6899
Younhun Kim <younhun@mit.edu>
2022-06-16 10:48:30 -0400

    Updated evaluate.sh

--- scripts/synthetic/evaluate.sh
+++ scripts/synthetic/evaluate.sh
@@ -4,4 +4,7 @@ source synthetic/settings.sh
 
 
 export PYTHONPATH=${CLV_DIR}
-bash synthetic/helpers/evaluate.py -g ${DATASET_DIR}/glv.npz
+bash synthetic/helpers/evaluate.py \
+-g ${DATASET_DIR}/glv.npz \
+-m 3723.06368786 \
+-s 6567.91725508
