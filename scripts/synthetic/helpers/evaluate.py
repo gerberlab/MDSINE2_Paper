eb727d14a348799398b55a07c589cda2134e7165
Younhun Kim <younhun@mit.edu>
2022-06-17 15:34:17 -0400

    fixed some bugs in evaluate.py

--- scripts/synthetic/helpers/evaluate.py
+++ scripts/synthetic/helpers/evaluate.py
@@ -270,10 +270,10 @@ def evaluate_topology_errors(true_indicators: np.ndarray, results_base_dir: Path
     df_entries = []
 
     def _false_positive_rate(pred, truth) -> float:
-        return np.sum(pred & np.logical_not(truth)) / truth.size()
+        return np.sum(pred & np.logical_not(truth)) / truth.size
 
     def _true_positive_rate(pred, truth) -> float:
-        raise np.sum(pred & truth) / truth.size()
+        raise np.sum(pred & truth) / truth.size
 
     for read_depth, trial_num, noise_level, result_dir in result_dirs(results_base_dir):
         def _compute_roc_curve(_method: str, _pvalues: np.ndarray):
