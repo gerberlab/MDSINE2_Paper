c4005d217816d7e802e022c1d6f472847a5f65b3
Younhun Kim <younhun@mit.edu>
2022-06-17 14:09:50 -0400

    added batch script for non-clustered run.

--- scripts/synthetic/evaluate.sh
+++ scripts/synthetic/evaluate.sh
@@ -4,6 +4,7 @@ source synthetic/settings.sh
 
 
 export PYTHONPATH=${CLV_DIR}
+echo "[*] Running evaluation script."
 bash synthetic/helpers/evaluate.py \
 -g ${DATASET_DIR}/glv.npz \
 -m 3723.06368786 \
