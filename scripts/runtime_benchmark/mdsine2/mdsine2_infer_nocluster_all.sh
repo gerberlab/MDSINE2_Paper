e4ab1ae54fa3e551871e6b573c484eca0bb938ae
Younhun Kim <younhun@mit.edu>
2022-06-17 12:26:20 -0400

    added batch script for non-clustered run.

---
+++ scripts/runtime_benchmark/mdsine2/mdsine2_infer_nocluster_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+source runtime_benchmark/settings.sh
+
+
+for n_taxa in 10 25 50 100; do
+	for (( trial = 1; trial < ${NUM_TRIALS}+1; trial++ )); do
+		bash runtime_benchmark/mdsine2/mdsine2_infer_nocluster.sh $n_taxa $trial
+	done
+done
