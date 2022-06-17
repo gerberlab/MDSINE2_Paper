4c10db50ad4cbaadeb977e48dced9413721cf6c3
Younhun Kim <younhun@mit.edu>
2022-06-17 14:11:13 -0400

    Fixed broken evaluate.sh

--- scripts/synthetic/evaluate.sh
+++ scripts/synthetic/evaluate.sh
@@ -6,6 +6,8 @@ source synthetic/settings.sh
 export PYTHONPATH=${CLV_DIR}
 echo "[*] Running evaluation script."
 python synthetic/helpers/evaluate.py \
+-r ${OUTPUT_DIR} \
+-o ${OUTPUT_DIR} \
 -g ${DATASET_DIR}/glv.npz \
 -m 3723.06368786 \
 -s 6567.91725508
