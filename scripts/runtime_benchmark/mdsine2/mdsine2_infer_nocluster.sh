51f0b37c0d8f4c16e5a1e556dd52bab305eca105
Younhun Kim <younhun@mit.edu>
2022-06-17 12:25:03 -0400

    Added non-clustered inference runs for runtime benchmarking.

--- scripts/runtime_benchmark/mdsine2/mdsine2_infer_nocluster.sh
+++ scripts/runtime_benchmark/mdsine2/mdsine2_infer_nocluster.sh
@@ -17,7 +17,7 @@ dataset=${DATASET_DIR}/trimmed_${n_taxa}/dataset.pkl
 
 trial_dir=${OUTPUT_DIR}/taxa_top_${n_taxa}/trial_${trial}
 negbin=${trial_dir}/mdsine2_negbin/replicates/mcmc.pkl
-inference_out_dir=${trial_dir}/mdsine2
+inference_out_dir=${trial_dir}/mdsine2_nocluster
 mkdir -p $inference_out_dir
 
 runtime_file=${inference_out_dir}/runtime.txt
@@ -31,19 +31,18 @@ echo "${seed}" > $seed_file
 echo "[*] Running mdsine2 inference (n_taxa=${n_taxa}, trial=${trial}; using seed=${seed})"
 start_time=$(date +%s%N)  # nanoseconds
 
-mdsine2 infer \
+python runtime_benchmark/helpers/mdsine2_nocluster.py \
 		--input $dataset \
 		--negbin $negbin \
 		--seed ${seed} \
 		--burnin 5000 \
 		--n-samples 15000 \
 		--checkpoint 1000 \
-		--multiprocessing 0 \
 		--basepath $inference_out_dir \
 		--interaction-ind-prior "strong-sparse" \
 		--perturbation-ind-prior "weak-agnostic"
 
 end_time=$(date +%s%N)
 elapsed_time=$(( $(($end_time-$start_time)) / 1000000 ))
-echo "[*] Finished mdsine2 inference. (${elapsed_time} ms)"
+echo "[*] Finished mdsine2 (NON-CLUSTERED) inference. (${elapsed_time} ms)"
 echo "${elapsed_time}" > $runtime_file
