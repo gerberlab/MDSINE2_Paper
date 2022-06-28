de8eb02197d8cf71ace89cae519c6d0d1b0304c4
Younhun Kim <younhun@mit.edu>
2022-06-28 11:15:52 -0400

    Updated rendering.

--- scripts/synthetic/helpers/render_evaluation.py
+++ scripts/synthetic/helpers/render_evaluation.py
@@ -87,6 +87,7 @@ def render_topology_errors(df: pd.DataFrame, text_ax, ax1, ax2):
 
     area_df = df.groupby(['Method', 'ReadDepth', 'NoiseLevel']).apply(auroc).reset_index()
     print(type(area_df))
+    print(area_df)
 
     area_df['x'] = area_df['ReadDepth'].astype(str) + ' reads\n' + area_df['NoiseLevel'].astype(str) + ' noise'
     sb.barplot(
