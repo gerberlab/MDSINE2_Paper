9c2a442ca0de23c6d8a3f0ca9602b397f0447192
Younhun Kim <younhun@mit.edu>
2022-07-05 11:41:09 -0400

    added negbin step to synthetic mdsine2 inference.

--- scripts/synthetic/mdsine2_infer_all.sh
+++ scripts/synthetic/mdsine2_infer_all.sh
@@ -5,16 +5,23 @@ source synthetic/settings.sh
 for read_depth in 1000 25000; do
 	for (( trial = 0; trial < ${NUM_SAMPLE_TRIALS}; trial++ )); do
 		for noise_level in "low" "medium" "high"; do
+			negbin_seed=123
 			dataset=${DATASET_DIR}/data/reads_${read_depth}/trial_${trial}/subjset_${noise_level}.pkl
 			trial_output_dir=${OUTPUT_DIR}/reads_${read_depth}/trial_${trial}/${noise_level}_noise
+
+			negbin_out_dir=${trial_output_dir}/mdsine2_negbin
 			inference_out_dir=${trial_output_dir}/mdsine2
 			mkdir -p $inference_out_dir
 
+			# ======= Fit NegBin qPCR model
+			echo "[*] Fitting Negative binomial model."
+			mdsine2 infer-negbin --input $dataset --seed ${negbin_seed} --burnin 2000 --n-samples 6000 --checkpoint 200 --basepath $negbin_out_dir
+
 			# ======= Run inference
-			echo "[*] Running mdsine2 inference (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
+			echo "[*] Running non-clustered mdsine2 inference (reads=${read_depth}, trial=${trial}, noise level=${noise_level})"
 			python synthetic/helpers/inference.py \
 					--input $dataset \
-					--negbin ${NEGBIN_A0} ${NEGBIN_A1} \
+					--negbin ${negbin_out_dir}/replicates/mcmc.pkl \
 					--seed 0 \
 					--burnin 5000 \
 					--n-samples 15000 \
