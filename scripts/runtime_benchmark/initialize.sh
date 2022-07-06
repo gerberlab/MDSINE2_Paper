343615648bd0139936d29612156cab18121945f7
Younhun Kim <younhun@mit.edu>
2022-07-06 12:10:04 -0400

    added custom filtering to runtime_benchmark.

--- scripts/runtime_benchmark/initialize.sh
+++ scripts/runtime_benchmark/initialize.sh
@@ -2,14 +2,27 @@
 set -e
 source runtime_benchmark/settings.sh
 
-
+require_program mdsine2
 require_program python
 
 cd runtime_benchmark/helpers
-study=${DATASET_DIR}/preprocessed/gibson_healthy_agg_taxa_filtered.pkl
+study=${DATASET_DIR}/preprocessed/gibson_healthy_agg_taxa.pkl
+filtered_study=${DATASET_DIR}/preprocessed/gibson_healthy_agg_taxa_filtered.pkl
 replicate_study=${DATASET_DIR}/preprocessed/gibson_replicates_agg_taxa.pkl
 
-for n_taxa in 10 25 50 100; do
+
+echo "[*] Filtering main dataset."
+mdsine2 filter \
+    --dataset $study \
+    --outfile $filtered_study \
+    --dtype rel \
+    --threshold 0.0001 \
+    --min-num-consecutive 5 \
+    --min-num-subjects 1 \
+    --colonization-time 5
+
+
+for n_taxa in 10 25 50 100 200; do
 	target_dir=${DATASET_DIR}/trimmed_${n_taxa}
 
 	echo "[*] Preparing dataset of ${n_taxa} taxa... (target dir = ${target_dir})"
@@ -18,7 +31,7 @@ for n_taxa in 10 25 50 100; do
 	target_dataset=${target_dir}/dataset.pkl
 	target_replicate=${target_dir}/replicates.pkl
 
-	python select_topk_bugs.py -f ${study} -n $n_taxa -o $target_dataset
+	python select_topk_bugs.py -f ${filtered_study} -n $n_taxa -o $target_dataset
 	python filter_replicates_like_other_dataset.py \
 			--replicate-dataset $replicate_study \
 			--like-other $target_dataset \
