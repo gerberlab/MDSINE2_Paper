7046e19c1cadfaf1ded93b3db8546aa488b891d3
Younhun Kim <younhun@mit.edu>
2022-07-07 16:15:37 -0400

    Raised cohort size to 10, num trials to 10 (synthetic)

--- scripts/synthetic/settings.sh
+++ scripts/synthetic/settings.sh
@@ -10,8 +10,8 @@ export MDSINE2_LOG_INI="${PROJECT_DIR}/scripts/synthetic/logging.ini"
 export GLV_PARAMS=${DATASET_DIR}/glv.npz
 export TIME_POINTS=${DATASET_DIR}/time_points.txt
 
-export NUM_SAMPLE_TRIALS=5
-export COHORT_SIZE=5
+export NUM_SAMPLE_TRIALS=10
+export COHORT_SIZE=10
 export PROCESS_VAR=0.01
 export SIMULATION_DT=0.01
 export LOW_NOISE_SCALE=0.01
