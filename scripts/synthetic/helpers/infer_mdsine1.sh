ea41e342e0975e699a88699ce1d564d69fe3991f
Younhun Kim <younhun@mit.edu>
2022-07-08 14:32:55 -0400

    removed interventions.

--- scripts/synthetic/helpers/infer_mdsine1.sh
+++ scripts/synthetic/helpers/infer_mdsine1.sh
@@ -194,7 +194,7 @@ python synthetic/helpers/create_mdsine1_inputs.py \
 -m metadata.txt \
 -c counts.txt \
 -b biomass.txt \
--t 0
+-t 11
 
 echo "[*] Running matlab implementation..."
 cd $MDSINE1_DIR
