da230ee7afaeb4da988d27fd3029e7e4914d2248
Younhun Kim <younhun@mit.edu>
2022-07-11 14:43:22 -0400

    Removed day 10 challenge (all taxa init at day 0)

--- scripts/synthetic/helpers/create_datasets.py
+++ scripts/synthetic/helpers/create_datasets.py
@@ -255,7 +255,7 @@ def main():
         init_dist=variables.Normal(initial_cond_mean, np.power(initial_cond_std, 2)),
         processvar=process_var,
         initial_min_value=100.0,
-        intervene_day=10.0
+        intervene_day=0.0
     )
 
     # Plot the trajectories.
