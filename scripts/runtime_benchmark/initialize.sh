34e37af49e369b8252008efa12ece1f9c96e37ae
Younhun Kim <younhun@mit.edu>
2022-08-09 15:25:47 -0400

    lowered filter threshold.

--- scripts/runtime_benchmark/initialize.sh
+++ scripts/runtime_benchmark/initialize.sh
@@ -17,9 +17,9 @@ mdsine2 filter \
     --outfile $filtered_study \
     --dtype rel \
     --threshold 0.0001 \
-    --min-num-consecutive 2 \
+    --min-num-consecutive 1 \
     --min-num-subjects 1 \
-    --colonization-time 2
+    --colonization-time 1
 
 
 for n_taxa in 10 25 50 100 200; do
