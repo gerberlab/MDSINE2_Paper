9d1061ddf0df33bcfdfeb0c18b5748cfbd8728cd
Younhun Kim <younhun@mit.edu>
2022-08-10 18:38:46 -0400

    wip

--- scripts/analysis/mdsine2_negbin.sh
+++ scripts/analysis/mdsine2_negbin.sh
@@ -2,10 +2,10 @@
 set -e
 source analysis/settings.sh
 
-echo "Learning negative binomial dispersion parameters..."
-echo "Output Directory: ${NEGBIN_OUT_DIR}"
-
 mkdir -p ${NEGBIN_OUT_DIR}
+
+echo "[*] Learning negative binomial dispersion parameters..."
+echo "Output Directory: ${NEGBIN_OUT_DIR}"
 
 mdsine2 infer-negbin \
     --input ${REPLICATE_DSET} \
@@ -15,6 +15,7 @@ mdsine2 infer-negbin \
     --checkpoint 200 \
     --basepath ${NEGBIN_OUT_DIR}
 
+echo "[*] Rendering negbin visualization."
 mdsine2 visualize-negbin \
     --chain ${REPLICATE_MCMC} \
     --output-basepath ${REPLICATE_PLOTS}
