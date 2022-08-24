f5584da22b83258ac0ca95e554e0a288acb58853
Younhun Kim <younhun@mit.edu>
2022-08-24 17:56:50 -0400

    changed mdsine2_infer to only run on seed 0. Added mdsine2_infer_fixedcluster.sh.

--- scripts/analysis/mdsine_infer_fixedcluster.sh
+++ scripts/analysis/mdsine_infer_fixedcluster.sh
@@ -2,14 +2,15 @@
 set -e
 source analysis/settings.sh
 
-echo "Running MDSINE2 model"
+echo "Running MDSINE2 model in fixed-clustering mode"
 echo "Writing files to ${MDSINE2_OUT_DIR}"
 
 # Healthy cohort
 # --------------
 
 seed=0
-study_name="healthy-seed${seed}"
+old_study_name="healthy-seed${seed}"
+study_name="healthy-seed${seed}-fixed-cluster"
 
 # Seed 0
 echo "[*] Performing MDSINE2 inference on ${study_name}"
@@ -24,11 +25,12 @@ mdsine2 infer \
 		--rename-study $study_name \
 		--basepath $MDSINE2_OUT_DIR \
 		--interaction-ind-prior $INTERACTION_IND_PRIOR \
-		--perturbation-ind-prior $PERTURBATION_IND_PRIOR
+		--perturbation-ind-prior $PERTURBATION_IND_PRIOR \
+		--fixed-clustering $MDSINE2_OUT_DIR/$old_study_name/mcmc.pkl
 
 echo "[*] Visualizing output of ${study_name}"
 mdsine2 visualize-posterior \
 		--chain $MDSINE2_OUT_DIR/$study_name/mcmc.pkl \
 		--output-basepath $MDSINE2_OUT_DIR/$study_name/posterior
 
-echo "Finished inference."
+echo "Finished inference (fixed-cluster mode)."
