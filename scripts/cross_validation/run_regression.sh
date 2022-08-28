a15b65c557a9f336fc09aec4c08b52f36f5cf8ea
Younhun Kim <younhun@mit.edu>
2022-08-27 23:10:04 -0400

    wip bugfix for cross validation

--- scripts/cross_validation/run_regression.sh
+++ scripts/cross_validation/run_regression.sh
@@ -5,7 +5,7 @@ source cross_validation/settings.sh
 model=$1
 reg=$2
 
-echo "[*] Running ${model}, regression ${reg}. Output dir=${CV_OUTPUT_DIR}"
+echo "[*] Running ${model}, regression ${reg}. Output dir=${OUTPUT_DIR}"
 echo "Using input dir: ${REGRESSION_DATASET_DIR}"
 python ${CLV_DIR}/healthy_prediction_experiments_cv.py \
 		-m "${model}" \
