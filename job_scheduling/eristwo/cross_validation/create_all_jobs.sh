cb2606c8cb075ef8a60a98217f50988e7af210fc
Younhun Kim <younhun@mit.edu>
2022-08-27 23:28:34 -0400

    changed log_every to log-every.

--- job_scheduling/eristwo/cross_validation/create_all_jobs.sh
+++ job_scheduling/eristwo/cross_validation/create_all_jobs.sh
@@ -9,11 +9,10 @@ bash create_regression_job.sh "glv" "elastic-net"
 bash create_regression_job.sh "glv-ra" "ridge"
 bash create_regression_job.sh "glv-ra" "elastic-net"
 bash create_regression_job.sh "lra" "elastic-net"
-bash create_mdsine2_job.sh 0
-bash create_mdsine2_job.sh 1
 bash create_mdsine2_job.sh 2
 bash create_mdsine2_job.sh 3
 bash create_mdsine2_job.sh 4
+bash create_mdsine2_job.sh 5
 
 
 run_script_path=${LSF_DIR}/submit_all.sh
