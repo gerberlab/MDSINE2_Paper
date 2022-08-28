9a50d9c722eb3b34af3076fb27e4fa1842565fc3
Younhun Kim <younhun@mit.edu>
2022-08-27 22:51:29 -0400

    wip bugfix for cross validation

--- scripts/cross_validation/initialize.sh
+++ scripts/cross_validation/initialize.sh
@@ -6,12 +6,12 @@ source cross_validation/settings.sh
 echo "[*] Preparing dataset for data (target: ${REGRESSION_DATASET_DIR})"
 mkdir -p $REGRESSION_DATASET_DIR
 
-python cv_comparator_methods/helpers/create_biomass_table.py \
+python cross_validation/helpers/create_biomass_table.py \
 	-s "${PREPROCESS_DIR}/gibson_healthy_agg_filtered.pkl" \
 	-o "${REGRESSION_DATASET_DIR}" \
 	-on "abundance"
 
-python cv_comparator_methods/helpers/format_data.py \
+python cross_validation/helpers/format_data.py \
 	-a "${REGRESSION_DATASET_DIR}/abundance.txt"\
 	-m "${CLV_DIR}/data/gibson/basic_info/metadata.txt"\
 	-o "${REGRESSION_DATASET_DIR}"
