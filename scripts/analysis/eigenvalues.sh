6f9111a587b67391b05990eeb3b575470a611ed7
Younhun Kim <younhun@mit.edu>
2022-10-12 15:14:12 -0400

    updated eigenvalue calculation.

--- scripts/analysis/eigenvalues.sh
+++ scripts/analysis/eigenvalues.sh
@@ -8,14 +8,7 @@ echo "Evaluating MDSINE2 learned model's eigenvalues (exclude clusters)"
 # Healthy cohort
 # --------------
 
-seed=0
-study="healthy-seed${seed}"
-fixed_module_study="healthy-seed${seed}-fixed-cluster"
-
-mcmc=$MDSINE2_OUT_DIR/${study}/mcmc.pkl
-fixed_module_mcmc=$MDSINE2_OUT_DIR/${fixed_module_study}/mcmc.pkl
-
-outdir=${MDSINE2_OUT_DIR}/${study}/eigenvalues
+outdir=${MDSINE2_OUT_DIR}/merged_studies/eigenvalues
 mkdir -p $outdir
 
 # Note: index 19 = day 20
