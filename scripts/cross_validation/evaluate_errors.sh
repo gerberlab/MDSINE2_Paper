ac102822af61ef699b0579f9ca3dad7154cf5e02
Younhun Kim <younhun@mit.edu>
2022-10-14 14:05:54 -0400

    updated CV with weak prior options.

--- scripts/cross_validation/evaluate_errors.sh
+++ scripts/cross_validation/evaluate_errors.sh
@@ -10,6 +10,7 @@ python cross_validation/helpers/evaluate_errors.py \
 --regression_inputs_dir ${REGRESSION_DATASET_DIR} \
 --mdsine_outdir ${OUTPUT_DIR}/mdsine2-modules \
 --mdsine_nomodule_outdir ${OUTPUT_DIR}/mdsine2-nomodules \
+--mdsine_weak_prior_outdir ${OUTPUT_DIR}/mdsine2-modules-weak-interaction-prior \
 --clv_elastic_outdir ${OUTPUT_DIR}/regression_clv_elastic-net \
 --glv_elastic_outdir ${OUTPUT_DIR}/regression_glv_elastic-net \
 --glv_ra_elastic_outdir ${OUTPUT_DIR}/regression_glv-ra_elastic-net \
