35eee4d71ddb635e783e3e1ff92ff87ac7cb8e26
Younhun Kim <younhun@mit.edu>
2022-11-03 16:24:27 -0400

    cv -- plot renders only weak prior runs for mdsine2.

--- scripts/cross_validation/helpers/evaluate_errors.py
+++ scripts/cross_validation/helpers/evaluate_errors.py
@@ -751,7 +751,8 @@ def main():
     )
 
     # ==================== Plot settings.
-    methods = ['MDSINE2', 'MDSINE2 (No Modules)', 'MDSINE2 (Weak Prior)', 'MDSINE2 (No Modules, Weak Prior)', 'cLV', 'LRA', 'gLV-RA (elastic net)', 'gLV-RA (ridge)', 'gLV (ridge)', 'gLV (elastic net)']
+    # methods = ['MDSINE2', 'MDSINE2 (No Modules)', 'MDSINE2 (Weak Prior)', 'MDSINE2 (No Modules, Weak Prior)', 'cLV', 'LRA', 'gLV-RA (elastic net)', 'gLV-RA (ridge)', 'gLV (ridge)', 'gLV (elastic net)']
+    methods = ['MDSINE2 (Weak Prior)', 'MDSINE2 (No Modules, Weak Prior)', 'cLV', 'LRA', 'gLV-RA (elastic net)', 'gLV-RA (ridge)', 'gLV (ridge)', 'gLV (elastic net)']
     palette_tab20 = sns.color_palette("tab10", len(methods))
     method_colors = {m: palette_tab20[i] for i, m in enumerate(methods)}
 
