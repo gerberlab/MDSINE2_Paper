fdcdda8b71b48a139369a2ad1d2327ed346c70c9
Younhun Kim <younhun@mit.edu>
2022-11-11 14:05:57 -0500

    wip

--- scripts/synthetic/helpers/create_datasets.py
+++ scripts/synthetic/helpers/create_datasets.py
@@ -309,6 +309,10 @@ def main():
     for subj in synthetic.subjs:
         fig, ax = plt.subplots(figsize=(10, 8))
         trajs = raw_trajs[subj]['X']  # (n_taxa) x (n_times)
+
+        print(times)
+        print(trajs)
+
         times = raw_trajs[subj]['times']
         for taxa_traj in trajs:
             ax.plot(times, taxa_traj, marker=None)
