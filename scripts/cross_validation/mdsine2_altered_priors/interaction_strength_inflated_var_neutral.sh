7d057bdf7d0fd9b587f664f91f5ad40670862791
Younhun Kim <younhun@mit.edu>
2023-11-20 14:00:49 -0500

    renamed directories for new scripts from previous commit.

--- scripts/cross_validation/mdsine2_altered_priors/interaction_strength_inflated_var_neutral.sh
+++ scripts/cross_validation/mdsine2_altered_priors/interaction_strength_inflated_var_neutral.sh
@@ -7,7 +7,7 @@ excluded_subj=$1
 
 
 seed=$((excluded_subj * 100))
-inference_out_dir=$OUTPUT_DIR/mdsine2-modules-interaction-strength-uninformed1/${excluded_subj}
+inference_out_dir=$OUTPUT_DIR/mdsine2-modules-var-neutral/${excluded_subj}
 mkdir -p ${inference_out_dir}
 
 echo "[*] Running cross-validation run for MDSINE2 with negative interaction-str prior (Excluding subject: ${excluded_subj})"
