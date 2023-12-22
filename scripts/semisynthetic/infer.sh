38fa45d3a4a77c63cc195f4b237595a482795447
Younhun Kim <younhun@mit.edu>
2023-12-22 14:10:34 -0500

    added semisynthetic inference script.

---
+++ scripts/semisynthetic/infer.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set -e
+source semisynthetic/settings.sh
+
+# Healthy cohort
+# --------------
+
+read_depth=$1
+trial=$2
+require_variable "read_depth" $read_depth
+require_variable "trial" $trial
+
+
+echo "[*] Performing MDSINE2 inference for semisynthetic (read_depth=${read_depth} | trial=${trial})"
+
+inference_dir=${DATASET_DIR}/read_depth_${read_depth}/trial_${trial}/inference
+mkdir -p ${inference_dir}
+
+breadcrumb=${inference_dir}/inference.DONE
+if [ -f ${breadcrumb} ]; then
+  echo "[**] Inference already done."
+  exit 0
+fi
+
+
+export MDSINE2_LOG_INI="${PROJECT_DIR}/scripts/analysis/logging_to_file.ini"
+export LOG_FILEPATH="${inference_dir}/inference.log"
+touch $LOG_FILEPATH
+
+synth_study=${DATASET_DIR}/read_depth_${read_depth}/trial_${trial}/dataset.pkl
+output_study_name="simulated"
+mdsine2 infer \
+		--input $synth_study \
+		--negbin $REPLICATE_MCMC \
+		--seed $INFERENCE_SEED \
+		--burnin $BURNIN \
+		--n-samples $N_SAMPLES \
+		--checkpoint $CHECKPOINT \
+		--multiprocessing $MULTIPROCESSING \
+		--rename-study "${output_study_name}" \
+		--basepath $inference_dir \
+		--interaction-ind-prior $INTERACTION_IND_PRIOR \
+		--perturbation-ind-prior $PERTURBATION_IND_PRIOR
+
+echo "[*] Visualizing output of ${output_study_name}"
+export LOG_FILEPATH="${inference_dir}/visualization.log"
+mdsine2 visualize-posterior \
+		--chain ${inference_dir}/${output_study_name}/mcmc.pkl \
+		--output-basepath ${inference_dir}/${output_study_name}/posterior
+
+echo "[*] Finished inference."
+touch ${breadcrumb}
--- scripts/semisynthetic/settings.sh
+++ scripts/semisynthetic/settings.sh
@@ -10,3 +10,13 @@ export NUM_SAMPLE_TRIALS=5
 
 export STUDY_PKL="${PROJECT_DIR}/datasets/gibson/preprocessed/gibson_healthy_agg_filtered.pkl"
 export FIXED_MODULE_MCMC="${PROJECT_DIR}/datasets/gibson/output/mdsine2/inference/merged_studies_fixed_cluster/mcmc.pkl"
+
+# inference settings
+export REPLICATE_MCMC="${PROJECT_DIR}/datasets/gibson/output/mdsine2/negbin/replicates/mcmc.pkl"
+export INFERENCE_SEED=31415
+export BURNIN=5000
+export N_SAMPLES=15000
+export CHECKPOINT=100
+export MULTIPROCESSING=0
+export INTERACTION_IND_PRIOR="strong-sparse"
+export PERTURBATION_IND_PRIOR="weak-agnostic"
