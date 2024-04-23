f954642e770af974c0d9967deaa3a5a110e0da85
Younhun Kim <youn.kim.3181@gmail.com>
2024-04-23 15:56:23 -0400

    updated settings for semisynthetic.

--- scripts/semisynthetic/settings.sh
+++ scripts/semisynthetic/settings.sh
@@ -3,11 +3,16 @@ export DATASET_NAME="semisynthetic"
 source ./settings.sh  # use parent settings (scripts/settings.sh)
 
 
-export QPCR_NOISE_SCALE=0.1 #0.2989
+export QPCR_NOISE_SCALE=0.2989  #0.2989
 export ALPHA_DIRICHLET=4511.6211 # old value: 5373.2041  # empirical fit using replicate data.
+
+# from real data
+export NEGBIN_A0=4.2034E-08
+export NEGBIN_A1=6.0537E-02
+
 export READ_DEPTHS=(3000 15000 75000)
 #export READ_DEPTHS=(3750 7500 15000 30000 60000)
-export NUM_SAMPLE_TRIALS=5
+export NUM_SAMPLE_TRIALS=10
 
 export STUDY_PKL="${PROJECT_DIR}/datasets/gibson/preprocessed/gibson_healthy_agg_filtered.pkl"
 export FIXED_MODULE_MCMC="${PROJECT_DIR}/datasets/gibson/output/mdsine2/inference/merged_studies_fixed_cluster/mcmc.pkl"
@@ -15,7 +20,7 @@ export FIXED_MODULE_MCMC="${PROJECT_DIR}/datasets/gibson/output/mdsine2/inferenc
 # inference settings
 #export REPLICATE_MCMC="${PROJECT_DIR}/datasets/gibson/output/mdsine2/negbin/replicates/mcmc.pkl"
 export NEGBIN_SEED=314159
-export INFERENCE_SEED=31415
+export INFERENCE_SEED=123456
 export BURNIN=5000
 export N_SAMPLES=15000
 export CHECKPOINT=100
