772f1e6dbb4492d571267276a5e487cce9176ac5
Younhun Kim <youn.kim.3181@gmail.com>
2024-05-31 15:42:26 -0400

    updated regression run script to leave breadcrumb and not terminate on failure (to execute all regression before terminating).

--- scripts/semisynthetic/regression/run_regression_filtered_normal.sh
+++ scripts/semisynthetic/regression/run_regression_filtered_normal.sh
@@ -5,8 +5,12 @@ source semisynthetic/settings.sh
 
 read_depth=$1
 trial=$2
+model=$3
+regression=$4
 require_variable "read_depth" $read_depth
 require_variable "trial" $trial
+require_variable "model" $model
+require_variable "regression" $regression
 
 
 
@@ -25,6 +29,7 @@ run_regression() {
 	regression_type=$2
 	echo "[*] Running ${model}, ${regression_type} (reads=${read_depth} | trial=${trial})"
 	out_dir=${regression_dir}/output/$model/$regression_type
+	breadcrumb=${regression_dir}/output/$model/$regression_type/regression.DONE
 	mkdir -p $out_dir
 	python ${CLV_DIR}/healthy_prediction_experiments.py \
 	  -m $model \
@@ -32,12 +37,14 @@ run_regression() {
 	  -o $out_dir \
 	  -i ${regression_input_dir} \
 	  --limit_of_detection 1.0
+	touch ${breadcrumb}
 }
 
 # ======= Run regression methods
+run_regression "${model}" "${regression}"
 #run_regression "clv" "elastic_net"
 #run_regression "lra" "elastic_net"
-run_regression "glv" "elastic_net"
-run_regression "glv" "ridge"
+#run_regression "glv" "elastic_net"
+#run_regression "glv" "ridge"
 #run_regression "glv-ra" "elastic_net"
 #run_regression "glv-ra" "ridge"
