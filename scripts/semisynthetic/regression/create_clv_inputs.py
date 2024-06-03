5463d560a9dfec17ed6ab95ee8d49331972111b5
Younhun Kim <youn.kim.3181@gmail.com>
2024-06-03 11:10:36 -0400

    changed create_clv_inputs.py to add rescaling for numerical stability. Now outputs a "rescale.np" output alongside the pickles -- this is necessary for post-hoc rescaling of interaction matrix.

--- scripts/semisynthetic/regression/create_clv_inputs.py
+++ scripts/semisynthetic/regression/create_clv_inputs.py
@@ -35,9 +35,12 @@ if __name__ == "__main__":
     zero_counts = 0
     total_counts = 0
 
+    maximum_post_rescale_target = 100.0
     n_perts = len(study.perturbations)
     for subj in study:
-        counts = subj.matrix()["abs"].T
+        abund = subj.matrix()["abs"].T
+        abund_rescale_factor = maximum_post_rescale_target / np.max(abund)
+
         times = subj.times
         perturb_ids = np.zeros((len(times), n_perts), dtype=int)
         for p_idx, pert in enumerate(study.perturbations):
@@ -46,18 +49,19 @@ if __name__ == "__main__":
             timepoint_indices, = np.where((times >= start) & (times <= end))
             perturb_ids[timepoint_indices, p_idx] = 1
 
-        assert len(times) == counts.shape[0]
-        assert counts.shape[1] == len(taxa)
+        assert len(times) == abund.shape[0]
+        assert abund.shape[1] == len(taxa)
 
-        zero_counts += np.sum(counts == 0)
-        total_counts += counts.size
-        Y.append(counts)
+        zero_counts += np.sum(abund == 0)
+        total_counts += abund.size
+        Y.append(abund * abund_rescale_factor)
         U.append(perturb_ids)
         T.append(times)
 
     print("Checker")
     print("len(Y):{}, len(U):{}, len(T):{}".format(len(Y), len(U), len(T)))
     print("Shape Y[0]:{}, Shape U[0]:{}, Shape T[0]:{}".format(Y[0].shape, U[0].shape, T[0].shape))
+    print("Rescale factor: {}".format(abund_rescale_factor))
 
     out_dir = Path(args.output_dir)
     with open(out_dir / "Y.pkl", "wb") as f:
@@ -66,3 +70,5 @@ if __name__ == "__main__":
         pkl.dump(U, f)
     with open(out_dir / "T.pkl", "wb") as f:
         pkl.dump(T, f)
+    with open(out_dir / "rescale.np", "wb") as f:
+        np.save(f, np.array(abund_rescale_factor))
