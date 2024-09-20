98a469b59394602c76598e0fa70cc5da947d1950
Younhun Kim <youn.kim.3181@gmail.com>
2024-09-20 11:55:33 -0400

    updated readme

--- scripts/semisynthetic2/README.md
+++ scripts/semisynthetic2/README.md
@@ -3,4 +3,7 @@ Order of scripts:
 1) pick_ground_truth.sh
 2) create_initial_conditions.sh
 3) generate_trajectories.sh
-4) 
+4) [branching points:]
+   a) create_dataset_tables_vary_mice.sh
+   b) create_Dataset_tables_vary_perts.sh
+5) 
--- scripts/semisynthetic2/generate_trajectories.sh
+++ scripts/semisynthetic2/generate_trajectories.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 
-n_perts=$1
-require_variable "n_perts" "${n_perts}"
-
-
-for (( i = 0; i < ${N_TRAJ_SEEDS}; ++i)); do
-  python semisynthetic2/python_helpers/simulate_trajectories.py \
-  -r "${REAL_DATA_PKL}" \
-  -n "${MAX_N_MICE}" \
-  -p "${n_perts} " \
-  -i "${DATASET_DIR}/trajectory_replicate_${i}/initial_condition.npy" \
-  -o "${DATASET_DIR}/trajectory_replicate_${i}/perts_${n_perts}" \
-  -g "${DATASET_DIR}/truth" \
-  -dt "${SIM_DT}" \
-  -sm "${SIM_MAX}"
+for n_perts in 0 1 2 3; do
+  for (( replicate = 0; replicate < ${N_TRAJ_SEEDS}; ++replicate)); do
+    echo "[* generate_trajectories.sh] Generating forward simulation trajectories (n_perts=${n_perts} | replicate=${replicate})"
+    python semisynthetic2/python_helpers/simulate_trajectories.py \
+    -r "${REAL_DATA_PKL}" \
+    -n "${MAX_N_MICE}" \
+    -p "${n_perts} " \
+    -i "${DATASET_DIR}/trajectory_replicate_${replicate}/initial_condition.npy" \
+    -o "${DATASET_DIR}/trajectory_replicate_${replicate}/perts_${n_perts}" \
+    -g "${DATASET_DIR}/truth" \
+    -dt "${SIM_DT}" \
+    -sm "${SIM_MAX}"
+  done
 done
