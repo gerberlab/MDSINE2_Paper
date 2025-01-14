dafe83f9bd61100af61121d46357d6d674fd7729
Younhun Kim <youn.kim.3181@gmail.com>
2025-01-14 14:56:39 -0500

    updated cross_validation/helpers/format_data.py: -sort subject IDs prior to conversion -directly reads from pickles instead of intermediate files.
    updated/fixed semisynthetic/regression/create_clv_inputs.py
    -rescale factor now uses "correct" function
    -subject_ordering.txt to mirror with CV code

--- scripts/cross_validation/helpers/format_data.py
+++ scripts/cross_validation/helpers/format_data.py
@@ -1,227 +1,77 @@
 #generates the data in a format that is compatible to work with the original
 #cLV code
-
 import numpy as np
 import pickle as pkl
-import os
-
-from scipy.interpolate import UnivariateSpline
-import matplotlib.pyplot as plt
 import argparse
 from pathlib import Path
 
+import mdsine2 as md2
+from lv_forward_sims import adjust_concentrations
+
 def parse_arguments():
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("-a", "--abundance_file", required=True)
-    parser.add_argument("-m", "--metadata_file", required=True)
-    parser.add_argument("-o", "--output_loc", required=True)
-
+    parser.add_argument("-i", "--mdsine2_study", required=True)
+    parser.add_argument("-o", "--output_dir", required=True)
     return parser.parse_args()
-
-def plot_trajectories(Y, T, output_dir, outfile):
-    import matplotlib.pyplot as plt
-    import matplotlib.gridspec as gridspec
-
-    def plot_bar(ax, y, time, unique_color_id, remaining_ids):
-        T = y.shape[0]
-        cm = plt.get_cmap("tab20c")
-        colors = [cm(i) for i in range(20)]
-        #time = np.array([t for t in range(T)])
-        widths = np.concatenate((time[1:] - time[:-1], [1])).astype(float)
-        widths[widths > 1] = 1
-
-        widths -= 1e-1
-
-        y_colors = y[:,unique_color_id]
-        ax.bar(time, y_colors[:,0], width=widths, color=colors[0], align="edge")
-        for j in range(1, y_colors.shape[1]):
-            ax.bar(time, y_colors[:,j], bottom=y_colors[:,:j].sum(axis=1), width=widths, color=colors[j], align="edge")
-
-        ax.bar(time, y[:,remaining_ids].sum(axis=1), bottom=y_colors.sum(axis=1), width=widths, color=colors[19], align="edge")
-        #ax.set_title("Relative Abundances", fontsize=10)
-        #ax.legend(prop={"size" : 4}, bbox_to_anchor=[-0.1,1.225], loc="upper left", ncol=4)
-
-    def find_top_ids(Y, n):
-        ntaxa = Y[0].shape[1]
-        rel_abun = np.zeros(ntaxa)
-        for y in Y:
-            tpts = y.shape[0]
-            denom = y.sum(axis=1,keepdims=True)
-            denom[denom == 0] = 1
-            p = y / denom
-            rel_abun += p.sum(axis=0) / tpts
-        ids = np.argsort(-rel_abun)
-        return np.sort(ids[:n]), np.sort(ids[n:])
-
-    N = len(Y)
-    top19_ids, remaining_ids = find_top_ids(Y, 19)
-    fig, ax = plt.subplots(nrows=N,ncols=1,figsize=(N,2*N))
-    for i in range(N):
-        denom = Y[i].sum(axis=1)
-        denom[denom == 0] = 1
-        plot_bar(ax[i], (Y[i].T / denom).T, T[i], top19_ids, remaining_ids)
-
-    outfile = os.path.splitext(outfile)[0]
-    plt.tight_layout()
-    plt.savefig(output_dir + "/" + outfile + ".pdf")
-    plt.close()
-
-
-def compute_breakpoints(effects):
-    """Break the spline when external perturbations occur."""
-    breakpoints = []
-    for u in effects:
-        in_perturb = False
-        v = []
-        for i,ut in enumerate(u):
-            if i == 0 or i == u.shape[0]-1:
-                v.append(1)
-                continue
-            if np.any(ut) > 0 and not in_perturb:
-                v.append(1)
-                in_perturb = True
-            elif np.any(ut) > 0 and in_perturb:
-                v.append(0)
-            elif np.all(ut) == 0 and in_perturb:
-                i = 1 if v[i-1] == 0 else 0
-                v.append(i)
-                in_perturb = False
-            else:
-                v.append(0)
-        v = np.array(v)
-        breakpoints.append(np.nonzero(v)[0])
-    return breakpoints
-
-
-def denoise(counts, t_pts, effects=None):
-    """Takes a sequence of counts at t_pts, and returns denoised estimates
-    of latent trajectories."""
-    ntaxa = counts[0].shape[1]
-    denoised_traj = []
-
-    if effects is not None:
-        breakpoints = compute_breakpoints(effects)
-        for c,t,b in zip(counts,t_pts,breakpoints):
-            denoised = np.zeros(c.shape)
-            mass = c.sum(axis=1,keepdims=True)
-            p = c / c.sum(axis=1,keepdims=True)
-            p[p==0] = 1e-5
-            p /= p.sum(axis=1,keepdims=True)
-            c = (mass.T*p.T).T
-            for i in range(ntaxa):
-                for j in range(1,b.size):
-                    start = b[j-1]
-                    end = b[j]+1
-                    k = 5 if end - start <= 3 else 5
-                    #k = 3
-                    f = UnivariateSpline(t[start:end],c[start:end,i],k=k)
-                    denoised[start:end,i] = f(t[start:end])
-            denoised[0] = c[0]
-            denoised = np.clip(denoised, np.min(denoised[denoised > 0]), np.inf)
-            denoised_traj.append(denoised)
-    else:
-        for c,t in zip(counts,t_pts):
-            denoised = np.zeros(c.shape)
-            k = 3 if t.shape[0] <= 5 else 5
-            for i in range(ntaxa):
-                f = UnivariateSpline(t,c[:,i],k=k)
-                denoised[:,i] = f(t)
-            denoised = np.clip(denoised, np.min(denoised[denoised > 0]), np.inf)
-            denoised /= denoised.sum(axis=1,keepdims=True)
-            denoised_traj.append(denoised)
-
-    return denoised_traj
-
-def format_U(U):
-
-    U_flat = np.concatenate([np.unique(u.flatten()).tolist() for u in U])
-    u_unique = np.sort(np.unique(U_flat))
-    u_unique = [u for u in u_unique if u!=0]
-    final_U = []
-    for u in U:
-        u_ = u.flatten()
-        new_u = np.zeros((u_.shape[0], len(u_unique)))
-        for i in range(len(u_unique)):
-            new_u[:,i] = np.where(u_== u_unique[i], 1, 0)
-        final_U.append(new_u)
-    return final_U
 
 
 if __name__ == "__main__":
-    sample_id_to_subject_id = {}
-    subject_id_time = {}
-    subject_id_u = {}
-
     args = parse_arguments()
-    metadata_file = args.metadata_file
-    if ".txt" not in metadata_file:
-        metadata_file = metadata_file + ".txt"
+    print(f"Converting data from {args.mdsine2_study} for usage in CLV.")
+    study = md2.Study.load(args.mdsine2_study)
+    taxa = study.taxa
 
-    abundance_file = args.abundance_file
-    if ".txt" not in abundance_file:
-        abundance_file = abundance_file + ".txt"
+    subject_counts = {
+        subj.name: subj.matrix()["abs"] for subj in study
+    }
 
-    with open(metadata_file, "r") as f:
-        for line in f:
-            line = line.split()
-            if "sampleID" in line[0]:
-                continue
-            sample_id = line[0]
-            subject_id = line[2]
-            day = float(line[3])
-            perturb = float(line[4])
+    Y = []
+    U = []
+    T = []
 
-            sample_id_to_subject_id[sample_id] = subject_id
-            subject_id_time[subject_id] = subject_id_time.get(subject_id, []) + [day]
-            subject_id_u[subject_id] = subject_id_u.get(subject_id, []) + [perturb]
+    maximum_post_rescale_target = 100.0
+    if study.perturbations is None:
+        n_perts = 0
+    else:
+        n_perts = len(study.perturbations)
 
-    counts = np.loadtxt(abundance_file, delimiter="\t", dtype=str, comments="!")
-    names = counts[1:, 0]
-    counts = counts[:, 1:]
+    subj_ordering = [str(x) for x in sorted(int(subj.name) for subj in study)]
+    print("Subject ordering for regression code: {}".format(subj_ordering))
+    for subj_name in subj_ordering:
+        subj = study[subj_name]
+        abund = subj.matrix()["abs"].T
 
-    subject_id_counts = {}
+        times = subj.times
+        perturb_ids = np.zeros((len(times), n_perts), dtype=int)
+        if study.perturbations is not None:
+            for p_idx, pert in enumerate(study.perturbations):
+                start = pert.starts[subj.name]
+                end = pert.ends[subj.name]
+                timepoint_indices, = np.where((times >= start) & (times < end))
+                perturb_ids[timepoint_indices, p_idx] = 1
 
-    for row in counts.T:
-        sample_id = row[0]
-        counts = row[1:].astype(float)
-        subject_id = sample_id_to_subject_id[sample_id]
+        assert len(times) == abund.shape[0]
+        assert abund.shape[1] == len(taxa)
 
-        if subject_id in subject_id_counts:
-            subject_id_counts[subject_id] = np.vstack( (subject_id_counts[subject_id], np.array(counts)) )
-        else:
-            subject_id_counts[subject_id] = np.array(counts)
+        Y.append(abund)
+        U.append(perturb_ids)
+        T.append(times)
 
-    Y_healthy = []
-    U_healthy = []
-    T_healthy = []
-    zero_counts = 0
-    total_counts = 0
-    for subject_id in sorted(subject_id_counts):
-        y = np.array(subject_id_counts[subject_id])
-        t = np.array(subject_id_time[subject_id])
-        u = np.array(subject_id_u[subject_id])
-        u = u.reshape((u.size, 1))
-        zero_counts += y[y == 0].size
-        total_counts += y.size
+    print("len(Y):{}, len(U):{}, len(T):{}".format(len(Y), len(U), len(T)))
+    print("Shape Y[0]:{}, Shape U[0]:{}, Shape T[0]:{}".format(Y[0].shape, U[0].shape, T[0].shape))
 
-        Y_healthy.append(y)
-        U_healthy.append(u)
-        T_healthy.append(t)
+    _, abund_rescale_factor = adjust_concentrations(Y)
+    print("Rescale factor: {}".format(abund_rescale_factor))
 
-    U_healthy = format_U(U_healthy)
-
-    print("Checker")
-    print("len(Y):{}, len(U):{}, len(T):{}".format(len(Y_healthy),
-         len(U_healthy), len(T_healthy)))
-    print("Shape Y[0]:{}, Shape U[0]:{}, Shape T[0]:{}".format(
-         Y_healthy[0].shape, U_healthy[0].shape, T_healthy[0].shape))
-
-    savepath = Path(args.output_loc)
-    savepath.mkdir(exist_ok=True, parents=True)
-
-    pkl.dump(Y_healthy, open(savepath / "Y.pkl", "wb"))
-    pkl.dump(U_healthy, open(savepath / "U.pkl", "wb"))
-    pkl.dump(T_healthy, open(savepath/ "T.pkl", "wb"))
-
-    print("Generated data that can be used in the cLV code")
+    out_dir = Path(args.output_dir)
+    with open(out_dir / "Y.pkl", "wb") as f:
+        pkl.dump(Y, f)
+    with open(out_dir / "U.pkl", "wb") as f:
+        pkl.dump(U, f)
+    with open(out_dir / "T.pkl", "wb") as f:
+        pkl.dump(T, f)
+    with open(out_dir / "rescale.np", "wb") as f:
+        np.save(f, np.array(abund_rescale_factor))
+    with open(out_dir / "subj_ordering.txt", "wt") as f:
+        for subj_name in subj_ordering:
+            f.write(f"{subj_name}\n")
--- scripts/semisynthetic/regression/create_clv_inputs.py
+++ scripts/semisynthetic/regression/create_clv_inputs.py
@@ -7,6 +7,7 @@ import argparse
 from pathlib import Path
 
 import mdsine2 as md2
+from lv_forward_sims import adjust_concentrations
 
 
 def parse_arguments():
@@ -17,10 +18,6 @@ def parse_arguments():
 
 
 if __name__ == "__main__":
-    sample_id_to_subject_id = {}
-    subject_id_time = {}
-    subject_id_u = {}
-
     args = parse_arguments()
     study = md2.Study.load(args.mdsine2_study)
     taxa = study.taxa
@@ -32,23 +29,17 @@ if __name__ == "__main__":
     Y = []
     U = []
     T = []
-    zero_counts = 0
-    total_counts = 0
 
     maximum_post_rescale_target = 100.0
-    print("HI")
     if study.perturbations is None:
         n_perts = 0
     else:
         n_perts = len(study.perturbations)
 
-    max_abund_all = 0.0
-    for subj in study:
-        abund = subj.matrix()["abs"].T
-        max_abund_all = np.max([max_abund_all, np.max(abund)])
-
-    abund_rescale_factor = maximum_post_rescale_target / max_abund_all
-    for subj in study:
+    subj_ordering = [str(x) for x in sorted(int(subj.name) for subj in study)]
+    print("Subject ordering for regression code: {}".format(subj_ordering))
+    for subj_name in subj_ordering:
+        subj = study[subj_name]
         abund = subj.matrix()["abs"].T
 
         times = subj.times
@@ -57,21 +48,20 @@ if __name__ == "__main__":
             for p_idx, pert in enumerate(study.perturbations):
                 start = pert.starts[subj.name]
                 end = pert.ends[subj.name]
-                timepoint_indices, = np.where((times >= start) & (times <= end))
+                timepoint_indices, = np.where((times >= start) & (times < end))
                 perturb_ids[timepoint_indices, p_idx] = 1
 
         assert len(times) == abund.shape[0]
         assert abund.shape[1] == len(taxa)
 
-        zero_counts += np.sum(abund == 0)
-        total_counts += abund.size
-        Y.append(abund * abund_rescale_factor)
+        Y.append(abund)
         U.append(perturb_ids)
         T.append(times)
 
-    print("Checker")
     print("len(Y):{}, len(U):{}, len(T):{}".format(len(Y), len(U), len(T)))
     print("Shape Y[0]:{}, Shape U[0]:{}, Shape T[0]:{}".format(Y[0].shape, U[0].shape, T[0].shape))
+
+    _, abund_rescale_factor = adjust_concentrations(Y)
     print("Rescale factor: {}".format(abund_rescale_factor))
 
     out_dir = Path(args.output_dir)
@@ -83,3 +73,6 @@ if __name__ == "__main__":
         pkl.dump(T, f)
     with open(out_dir / "rescale.np", "wb") as f:
         np.save(f, np.array(abund_rescale_factor))
+    with open(out_dir / "subj_ordering.txt", "wt") as f:
+        for subj_name in subj_ordering:
+            f.write(f"{subj_name}\n")
