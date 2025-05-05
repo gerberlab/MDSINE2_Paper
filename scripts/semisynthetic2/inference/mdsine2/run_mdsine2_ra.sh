5ebcba7cc5434c8b6b1f8a13bd364056ec8aacf8
Younhun Kim <youn.kim.3181@gmail.com>
2025-05-05 16:31:55 -0400

    made mdsine2_ra implementation for semisynthetic2

--- scripts/semisynthetic2/inference/mdsine2/run_mdsine2_ra.sh
+++ scripts/semisynthetic2/inference/mdsine2/run_mdsine2_ra.sh
@@ -6,12 +6,12 @@ require_variable "inference_dir" "${inference_dir}"
 
 
 # ======= Fit NegBin qPCR model
-echo "[* run_mdsine2.sh] Fitting Negative binomial model."
+echo "[* run_mdsine2_ra.sh] Fitting Negative binomial model."
 replicate_study=${inference_dir}/synthetic_replicate_filtered.pkl
 mdsine2 infer-negbin --input "${replicate_study}" --seed "${NEGBIN_SEED}" --burnin 2000 --n-samples 6000 --checkpoint 200 --basepath "${inference_dir}"
 
 # ======= Perform inference
-echo "[* run_mdsine2.sh] Performing inference on dataset."
+echo "[* run_mdsine2_ra.sh] Performing inference on dataset."
 python semisynthetic2/inference/mdsine2/helpers/mdsine2_ra.py \
 		--input "${inference_dir}/synthetic_filtered.pkl" \
 		--negbin "${inference_dir}/synthetic-replicate/mcmc.pkl" \
